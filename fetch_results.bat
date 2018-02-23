fb992989efe6c47bed1055bada2222218895b5df
Arne Mayer <arne.mayer@student.hpi.uni-potsdam.de>
2018-02-23 01:43:36 +0100

    scripts to fetch results from pella

---
+++ fetch_results.bat
@@ -0,0 +1,1 @@
+scp -r Arne.Mayer@pella.eaalab.hpi.uni-potsdam.de:/home/Arne.Mayer/dev/hyrise/build/results .
---
+++ fetch_results.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+scp -r Arne.Mayer@pella.eaalab.hpi.uni-potsdam.de:/home/Arne.Mayer/dev/hyrise/build/results ./results
